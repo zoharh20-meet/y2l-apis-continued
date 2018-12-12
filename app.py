fd66395cd474659c0e62239a5a7b28f88e894ffd
Subhi Beidas <subhi@goshippo.com>
2018-12-13 00:38:44 +0200

    added app skeleton

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/study_image', methods = ['POST'])
+def study_image():
+    
+    image_url = request.form['url-input']
+    # At this point you have the image_url value from the front end
+    # Your job now is to send this information to the Clarifai API
+    # and read the result, make sure that you read and understand the
+    # example we covered in the slides! 
+
+    # YOUR CODE HERE!
+    
+    return render_template('home.html', results="No results yet :(")
+
+if __name__ == '__main__':
+    app.run(debug=True)
